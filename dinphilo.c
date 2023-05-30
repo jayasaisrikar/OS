6457fc8a565291590100702e758c866292f7a62a
JAYA SAI SRIKAR <98835978+jayasaisrikar@users.noreply.github.com>
2023-05-30 23:46:39 +0530

    Update dinphilo.c

--- dinphilo.c
+++ dinphilo.c
@@ -6,77 +6,84 @@
 #define NUM_PHILOSOPHERS 5
 #define LEFT (i + NUM_PHILOSOPHERS - 1) % NUM_PHILOSOPHERS
 #define RIGHT (i + 1) % NUM_PHILOSOPHERS
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
 
-enum { THINKING, HUNGRY, EATING } state[NUM_PHILOSOPHERS];
+int state[NUM_PHILOSOPHERS];
 pthread_mutex_t mutex;
-pthread_cond_t cond[NUM_PHILOSOPHERS];
+pthread_cond_t condition[NUM_PHILOSOPHERS];
 
-void test(int i)
-{
-    if (state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING) {
-        state[i] = EATING;
+void *philosopher(void *arg);
+void pick_forks(int i);
+void put_forks(int i);
+void test(int i);
+
+void *philosopher(void *arg) {
+    int i = *((int *) arg);
+    while (1) {
+        printf("Philosopher %d is thinking.\n", i);
+        sleep(rand() % 3 + 1);  // Thinking for a random time
+
+        pick_forks(i);
+
         printf("Philosopher %d is eating.\n", i);
-        pthread_cond_signal(&cond[i]);
+        sleep(rand() % 3 + 1);  // Eating for a random time
+
+        put_forks(i);
     }
 }
 
-void pickup_forks(int i)
-{
+void pick_forks(int i) {
     pthread_mutex_lock(&mutex);
     state[i] = HUNGRY;
     printf("Philosopher %d is hungry.\n", i);
     test(i);
-    if (state[i] != EATING)
-        pthread_cond_wait(&cond[i], &mutex);
+    if (state[i] != EATING) {
+        pthread_cond_wait(&condition[i], &mutex);
+    }
     pthread_mutex_unlock(&mutex);
 }
 
-void return_forks(int i)
-{
+void put_forks(int i) {
     pthread_mutex_lock(&mutex);
     state[i] = THINKING;
-    printf("Philosopher %d is thinking.\n", i);
+    printf("Philosopher %d has finished eating and is thinking.\n", i);
     test(LEFT);
     test(RIGHT);
     pthread_mutex_unlock(&mutex);
 }
 
-void *philosopher(void *arg)
-{
-    int i = *(int *)arg;
-    int eat_count = 0;
-
-    while (eat_count < 3) {
-        usleep(rand() % 1000000); // Think for a random period
-        pickup_forks(i);
-        usleep(rand() % 1000000); // Eat for a random period
-        return_forks(i);
-        eat_count++;
+void test(int i) {
+    if (state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING) {
+        state[i] = EATING;
+        printf("Philosopher %d has picked up forks %d and %d and is eating.\n", i, LEFT, i);
+        pthread_cond_signal(&condition[i]);
     }
-
-    pthread_exit(NULL);
 }
 
-int main()
-{
-    pthread_t philosophers[NUM_PHILOSOPHERS];
-    int philosopher_ids[NUM_PHILOSOPHERS];
+int main() {
+    pthread_t threads[NUM_PHILOSOPHERS];
+    int thread_ids[NUM_PHILOSOPHERS];
 
     pthread_mutex_init(&mutex, NULL);
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++)
-        pthread_cond_init(&cond[i], NULL);
-
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        philosopher_ids[i] = i;
-        pthread_create(&philosophers[i], NULL, philosopher, &philosopher_ids[i]);
+        pthread_cond_init(&condition[i], NULL);
     }
 
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++)
-        pthread_join(philosophers[i], NULL);
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+        thread_ids[i] = i;
+        pthread_create(&threads[i], NULL, philosopher, &thread_ids[i]);
+    }
+
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
     pthread_mutex_destroy(&mutex);
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++)
-        pthread_cond_destroy(&cond[i]);
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+        pthread_cond_destroy(&condition[i]);
+    }
 
     return 0;
 }
