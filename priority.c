4d8a377877696451860f25e184cd18f52f2aac7d
JAYA SAI SRIKAR <98835978+jayasaisrikar@users.noreply.github.com>
2023-05-30 21:02:46 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,57 +1,74 @@
 #include<stdio.h>
-int main()
-{
-    int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
-    printf("Enter Total Number of Process:");
-    scanf("%d",&n);
-    printf("\nEnter Burst Time and Priority\n");
-    for(i=0;i<n;i++)
-    {
-        printf("\nP[%d]\n",i+1);
-        printf("Burst Time:");
-        scanf("%d",&bt[i]);
-        printf("Priority:");
-        scanf("%d",&pr[i]);
-        p[i]=i+1;  
-    }
+struct process{
+int at,st,bt,wt,ft,tat,pri;
+char name[2];
+};
+int main(){
+int i,j,n;
+float awt,atat;
+printf("enter the number of process :");
+scanf("%d",&n);
+struct process p[n+1];
+for(i=0;i<n;i++){
+printf("enter the name,arrival time, burst time and priority of process %d  :",i+1);
+scanf("%s %d %d %d",p[i].name,&p[i].at,&p[i].bt,&p[i].pri);
+}
 
-    for(i=0;i<n;i++)
-    {
-        pos=i;
-        for(j=i+1;j<n;j++)
-        {
-            if(pr[j]<pr[pos])
-                pos=j;
-        }
-        temp=pr[i];
-        pr[i]=pr[pos];
-        pr[pos]=temp;
-        temp=bt[i];
-        bt[i]=bt[pos];
-        bt[pos]=temp;
-        temp=p[i];
-        p[i]=p[pos];
-        p[pos]=temp;
-    }
-    wt[0]=0;
-    for(i=1;i<n;i++)
-    {
-        wt[i]=0;
-        for(j=0;j<i;j++)
-            wt[i]+=bt[j];
-        total+=wt[i];
-    }
-    avg_wt=total/n;
-    total=0;
-    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
-    for(i=0;i<n;i++)
-    {
-        tat[i]=bt[i]+wt[i];  
-        total+=tat[i];
-        printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
-    }
-    avg_tat=total/n;
-    printf("\n\nAverage Waiting Time=%d",avg_wt);
-    printf("\nAverage Turnaround Time=%d\n",avg_tat);
-return 0;
-} 
+for(i=0;i<n-1;i++){
+for(j=i+1;j<n;j++){
+if(p[i].at>p[j].at){
+p[n]=p[i];
+p[i]=p[j];
+p[j]=p[n];
+}
+}
+}
+
+for(i=0;i<n-1;i++){
+for(j=i+1;j<n;j++){
+if(p[j].at==p[i].ft && p[j].pri<p[i].pri){
+p[n]=p[i];
+p[i]=p[j];
+p[j]=p[n];
+}
+}
+}
+
+awt=0;
+atat=0;
+for(i=0;i<n;i++){
+
+if(i==0){
+p[i].st=p[i].at;
+}
+else{
+int min=i;
+for(j=i;j<n;j++){
+if(p[j].pri<p[min].pri && p[j].at<=p[i-1].ft){
+min=j;
+}
+}
+p[n]=p[i];
+p[i]=p[min];
+p[min]=p[n];
+
+if(p[i].at<=p[i-1].ft)
+ p[i].st=p[i-1].ft;
+else
+ p[i].st=p[i].at;
+}
+p[i].wt=p[i].st-p[i].at;
+p[i].ft=p[i].st+p[i].bt;
+p[i].tat=p[i].ft-p[i].at;
+awt+=p[i].wt;
+atat+=p[i].tat;
+
+}
+
+printf("name	at	bt	wt	ft	tat	pri\n");
+for(i=0;i<n;i++){
+printf("%s	%d	%d	%d	%d	%d	%d\n",p[i].name,p[i].at,p[i].bt,p[i].wt,p[i].ft,p[i].tat,p[i].pri);
+}
+printf("average waiting time	=	%f\n",awt/n);
+printf("average turn around time	=	%f\n",atat/n);
+}
