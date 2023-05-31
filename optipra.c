b726f6ff85fdef363d71615f445592bb07fee435
JAYA SAI SRIKAR <98835978+jayasaisrikar@users.noreply.github.com>
2023-05-31 07:22:33 +0530

    Update optipra.c

--- optipra.c
+++ optipra.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
+
 int main()
 {
-int referenceString[10], pageFaults = 0, m, n, s, pages, frames;
-printf("\nEnter the number of Pages:\t");
-scanf("%d", &pages);
-printf("\nEnter reference string values:\n");
-for (m = 0; m < pages; m++)
+    int referenceString[10], pageFaults = 0, hits = 0, m, n, s, pages, frames;
+    printf("\nEnter the number of Pages:\t");
+    scanf("%d", &pages);
+    printf("\nEnter reference string values:\n");
+    
+    for (m = 0; m < pages; m++)
     {
         printf("Value No. [%d]:\t", m + 1);
         scanf("%d", &referenceString[m]);
     }
+    
     printf("\nWhat is the total number of frames:\t");
     scanf("%d", &frames);
+    
     int temp[frames];
     for (m = 0; m < frames; m++)
     {
         temp[m] = -1;
-    }  
+    }
+    
     for (m = 0; m < pages; m++)
     {
         s = 0;
@@ -26,10 +31,13 @@ for (m = 0; m < pages; m++)
             {
                 s++;
                 pageFaults--;
+                hits++;
                 break;
             }
         }
+        
         pageFaults++;
+        
         if ((pageFaults <= frames) && (s == 0))
         {
             temp[m] = referenceString[m];
@@ -55,23 +63,33 @@ for (m = 0; m < pages; m++)
                         break;
                     }
                 }
+                
                 if (found == 0)
                 {
                     index = n;
                     break;
                 }
             }
+            
             if (index != -1)
             {
                 temp[index] = referenceString[m];
             }
         }
+        
         printf("\n");
         for (n = 0; n < frames; n++)
         {
             printf("%d\t", temp[n]);
         }
     }
+    
+    float hitRatio = (float)hits / pages;
+    float missRatio = (float)(pageFaults - hits) / pages;
+    
     printf("\nTotal Page Faults:\t%d\n", pageFaults);
+    printf("Hit Ratio:\t%.2f\n", hitRatio);
+    printf("Miss Ratio:\t%.2f\n", missRatio);
+    
     return 0;
 }
