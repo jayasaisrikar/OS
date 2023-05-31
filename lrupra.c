1c3c1d6a5653f82b7325ff8dd279b190145662a2
JAYA SAI SRIKAR <98835978+jayasaisrikar@users.noreply.github.com>
2023-05-31 07:34:35 +0530

    Update lrupra.c

--- lrupra.c
+++ lrupra.c
@@ -1,58 +1,81 @@
-#include<stdio.h>
- int findLRU(int time[], int n){
-int i, minimum = time[0], pos = 0;
-for(i = 1; i < n; ++i){
-if(time[i] < minimum){
-minimum = time[i];
-pos = i;
-}}
-return pos;
-}
+#include <stdio.h>
 int main()
 {
-int no_of_frames, no_of_pages, frames[10], pages[30], counter = 0, time[10], flag1, flag2, i, j, pos, faults = 0;
-printf("Enter number of frames: ");
-scanf("%d", &no_of_frames);
-printf("Enter number of pages: ");
-scanf("%d", &no_of_pages);
-printf("Enter reference string: ");
-    for(i = 0; i < no_of_pages; ++i){
-     scanf("%d", &pages[i]);
+    int referenceString[10], pageFaults = 0, hits = 0, m, n, s, pages, frames;
+    printf("\nEnter the number of Pages:\t");
+    scanf("%d", &pages);
+    printf("\nEnter reference string values:\n");
+    for (m = 0; m < pages; m++)
+    {
+        printf("Value No. [%d]:\t", m + 1);
+        scanf("%d", &referenceString[m]);
     }
-    for(i = 0; i < no_of_frames; ++i){
-     frames[i] = -1;
+    printf("\nWhat are the total number of frames:\t");
+    scanf("%d", &frames);
+    int temp[frames];
+    int time[frames];
+    int currentTime = 0;
+    for (m = 0; m < frames; m++)
+    {
+        temp[m] = -1;
+        time[m] = 0;
     }
-     for(i = 0; i < no_of_pages; ++i){
-     flag1 = flag2 = 0;
-     for(j = 0; j < no_of_frames; ++j){
-     if(frames[j] == pages[i]){
-     counter++;
-     time[j] = counter;
-   flag1 = flag2 = 1;
-   break;
-   }}
- if(flag1 == 0){
-for(j = 0; j < no_of_frames; ++j){
-     if(frames[j] == -1){
-     counter++;
-     faults++;
-     frames[j] = pages[i];
-     time[j] = counter;
-     flag2 = 1;
-     break;
-     }}}
-     if(flag2 == 0){
-     pos = findLRU(time, no_of_frames);
-     counter++;
-     faults++;
-     frames[pos] = pages[i];
-     time[pos] = counter;
-     }
-    printf("\n");
-     for(j = 0; j < no_of_frames; ++j){
-     printf("%d\t", frames[j]);
-     }
-}
-printf("\n\nTotal Page Faults = %d", faults);
- return 0;
+    for (m = 0; m < pages; m++)
+    {
+        s = 0;
+        for (n = 0; n < frames; n++)
+        {
+            if (referenceString[m] == temp[n])
+            {
+                s++;
+                pageFaults--;
+                hits++;
+                time[n] = currentTime++;
+                break;
+            }
+        }
+        pageFaults++;
+        if ((pageFaults <= frames) && (s == 0))
+        {
+            int minTime = time[0];
+            int minIndex = 0;
+            for (n = 1; n < frames; n++)
+            {
+                if (time[n] < minTime)
+                {
+                    minTime = time[n];
+                    minIndex = n;
+                }
+            }
+            temp[minIndex] = referenceString[m];
+            time[minIndex] = currentTime++;
+        }
+        else if (s == 0)
+        {
+            int minTime = time[0];
+            int minIndex = 0;
+
+            for (n = 1; n < frames; n++)
+            {
+                if (time[n] < minTime)
+                {
+                    minTime = time[n];
+                    minIndex = n;
+                }
+            }
+            temp[minIndex] = referenceString[m];
+            time[minIndex] = currentTime++;
+        }
+        printf("\n");
+        for (n = 0; n < frames; n++)
+        {
+            printf("%d\t", temp[n]);
+        }
+    }
+    float hitRatio = (float)hits / pages;
+    float missRatio = (float)(pageFaults - hits) / pages;
+    printf("\nTotal Page Faults:\t%d\n", pageFaults);
+    printf("Hit Ratio:\t%.2f\n", hitRatio);
+    printf("Miss Ratio:\t%.2f\n", missRatio);
+    return 0;
 }
