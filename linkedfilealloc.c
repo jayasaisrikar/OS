96905646ff09faf17b6f28fd4eb91c3dd5be298b
JAYA SAI SRIKAR <98835978+jayasaisrikar@users.noreply.github.com>
2023-05-31 09:13:52 +0530

    Update linkedfilealloc.c

--- linkedfilealloc.c
+++ linkedfilealloc.c
@@ -1,45 +1,73 @@
-#include<stdio.h>
-#include<stdlib.h>
-void main()
-{
-int f[50], p,i, st, len, j, c, k, a;
-clrscr();
-for(i=0;i<50;i++)
-f[i]=0;
-printf("Enter how many blocks already allocated: ");
-scanf("%d",&p);
-printf("Enter blocks already allocated: ");
-for(i=0;i<p;i++)
-{
-scanf("%d",&a);
-f[a]=1;
-}
-x: printf("Enter index starting block and length: ");
-scanf("%d%d", &st,&len);
-k=len;
-if(f[st]==0)
-{
-for(j=st;j<(st+k);j++)
-{
-if(f[j]==0)
-{
-f[j]=1;
-printf("%d-------->%d\n",j,f[j]);
-}
-else
-{
-printf("%d Block is already allocated \n",j);
-k++;
-}
-}
-}
-else
-printf("%d starting block is already allocated \n",st);
-printf("Do you want to enter more file(Yes - 1/No - 0)");
-scanf("%d", &c);
-if(c==1)
-goto x;
-else
-exit(0);
+#include <stdio.h>
+#include <stdlib.h>
 
+struct Node {
+    int start;
+    int length;
+    struct Node* next;
+};
+
+void allocateFile(struct Node** head, int start, int length) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->start = start;
+    newNode->length = length;
+    newNode->next = NULL;
+    
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        struct Node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+int isFileAllocated(struct Node* head, int start, int length) {
+    struct Node* current = head;
+    while (current != NULL) {
+        if (current->start == start && current->length == length) {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
+}
+
+void displayFiles(struct Node* head) {
+    if (head == NULL) {
+        printf("No files allocated.\n");
+    } else {
+        printf("Files Allocated are:\n");
+        struct Node* current = head;
+        while (current != NULL) {
+            printf("Start: %d\tLength: %d\n", current->start, current->length);
+            current = current->next;
+        }
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+    int st, len, c;
+    
+    do {
+        printf("Enter starting block and length of file: ");
+        scanf("%d %d", &st, &len);
+        
+        if (isFileAllocated(head, st, len)) {
+            printf("The file is already allocated.\n");
+        } else {
+            allocateFile(&head, st, len);
+            printf("The file is allocated to disk.\n");
+        }
+        
+        printf("Do you want to enter more files? (Yes - 1/No - 0): ");
+        scanf("%d", &c);
+    } while (c == 1);
+    
+    displayFiles(head);
+    
+    return 0;
 }
