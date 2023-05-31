a11510abde93ac1d8e923cae40e09bf16bb7158f
JAYA SAI SRIKAR <98835978+jayasaisrikar@users.noreply.github.com>
2023-05-31 07:21:11 +0530

    Add files via upload

---
+++ optipra.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+int main()
+{
+int referenceString[10], pageFaults = 0, m, n, s, pages, frames;
+printf("\nEnter the number of Pages:\t");
+scanf("%d", &pages);
+printf("\nEnter reference string values:\n");
+for (m = 0; m < pages; m++)
+    {
+        printf("Value No. [%d]:\t", m + 1);
+        scanf("%d", &referenceString[m]);
+    }
+    printf("\nWhat is the total number of frames:\t");
+    scanf("%d", &frames);
+    int temp[frames];
+    for (m = 0; m < frames; m++)
+    {
+        temp[m] = -1;
+    }  
+    for (m = 0; m < pages; m++)
+    {
+        s = 0;
+        for (n = 0; n < frames; n++)
+        {
+            if (referenceString[m] == temp[n])
+            {
+                s++;
+                pageFaults--;
+                break;
+            }
+        }
+        pageFaults++;
+        if ((pageFaults <= frames) && (s == 0))
+        {
+            temp[m] = referenceString[m];
+        }
+        else if (s == 0)
+        {
+            int index = -1;
+            int furthest = m + 1;
+            
+            for (n = 0; n < frames; n++)
+            {
+                int found = 0;
+                for (int k = m + 1; k < pages; k++)
+                {
+                    if (temp[n] == referenceString[k])
+                    {
+                        found = 1;
+                        if (k > furthest)
+                        {
+                            furthest = k;
+                            index = n;
+                        }
+                        break;
+                    }
+                }
+                if (found == 0)
+                {
+                    index = n;
+                    break;
+                }
+            }
+            if (index != -1)
+            {
+                temp[index] = referenceString[m];
+            }
+        }
+        printf("\n");
+        for (n = 0; n < frames; n++)
+        {
+            printf("%d\t", temp[n]);
+        }
+    }
+    printf("\nTotal Page Faults:\t%d\n", pageFaults);
+    return 0;
+}
